500318d009c4a2b4f5214ea399cb38fb8d74b45f
Iagocp <114144454+Iagocp@users.noreply.github.com>
2023-09-15 10:29:38 +0200

    Add files via upload

---
+++ PSP_1Eval/src/pbuild/Pbuild1.java
@@ -0,0 +1,24 @@
+package pbuild;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class Pbuild1 {
+
+	public static void main(String[] arg) throws IOException {
+		// creating list of process
+		List<String> list = new ArrayList<String>();
+		list.add("notepad.exe");
+
+		// create the process
+		ProcessBuilder build = new ProcessBuilder(list);
+
+		// checking the command i list
+		System.out.println("command: " + build.command());
+		
+		//depues de cheear el proceso lo inicializo
+		build.start();
+
+	}
+}
